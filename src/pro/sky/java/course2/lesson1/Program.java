7f79aa79c74cd68aa5361155feeca23dc31f3455
askaretc <askaretc@yandex.ru>
2022-12-18 13:29:13 +0300

    Lesson 1 edited

--- src/pro/sky/java/course2/lesson1/Program.java
+++ src/pro/sky/java/course2/lesson1/Program.java
@@ -55,7 +55,7 @@ public class Program {
         car1.productionYear = 2015;
         car1.engineVolume = 1.7;
         car1.color = "yellow";
-        car1.getInfo();
+        System.out.println(car1);
 
         Car car2 = new Car();
         car2.brand = "Audi";
@@ -64,7 +64,7 @@ public class Program {
         car2.productionYear = 2020;
         car2.engineVolume = 3.0;
         car2.color = "black";
-        car2.getInfo();
+        System.out.println(car2);
 
         Car car3 = new Car();
         car3.brand = "BMW";
@@ -73,7 +73,7 @@ public class Program {
         car3.productionYear = 2021;
         car3.engineVolume = 3.0;
         car3.color = "black";
-        car3.getInfo();
+        System.out.println(car3);
 
         Car car4 = new Car();
         car4.brand = "Kia";
@@ -82,7 +82,7 @@ public class Program {
         car4.productionYear = 2018;
         car4.engineVolume = 2.4;
         car4.color = "red";
-        car4.getInfo();
+        System.out.println(car4);
 
         Car car5 = new Car();
         car5.brand = "Hyundai";
@@ -91,7 +91,7 @@ public class Program {
         car5.productionYear = 2016;
         car5.engineVolume = 1.6;
         car5.color = "orange";
-        car5.getInfo();
+        System.out.println(car5);
 
         System.out.println();
 
@@ -122,19 +122,19 @@ public class Program {
         System.out.println("Task 2\n");
 
         Car car6 = new Car("Lada", "Grande", "Russia", 2015, 1.7, "yellow");
-        car6.getInfo();
+        System.out.println(car6);
 
         Car car7 = new Car("Audi", "A8 50 L TDI quattro", "Germany", 2020, 3.0, "black");
-        car7.getInfo();
+        System.out.println(car7);
 
         Car car8 = new Car("BMW", "Z8", "Germany", 2021, 3.0, "black");
-        car8.getInfo();
+        System.out.println(car8);
 
         Car car9 = new Car("Kia", "Sportage 4 generation", "South Korea", 2018, 2.4, "red");
-        car9.getInfo();
+        System.out.println(car9);
 
         Car car10 = new Car("Hyundai", "Avante", "South Korea", 2016, 1.6, "orange");
-        car10.getInfo();
+        System.out.println(car10);
 
         System.out.println();
 
@@ -151,6 +151,9 @@ public class Program {
         Human human10 = new Human();
         human10.getAcquainted();
 
+        Human human11 = new Human("Владимир", "", 0, "");
+        human11.getAcquainted();
+
         System.out.println();
 
 
@@ -159,6 +162,9 @@ public class Program {
         System.out.println("Task 2\n");
 
         Car car11 = new Car();
-        car11.getInfo();
+        System.out.println(car11);
+
+        Car car12 = new Car("Ford", "", "USA", 0, 0,"");
+        System.out.println(car12);
     }
 }
