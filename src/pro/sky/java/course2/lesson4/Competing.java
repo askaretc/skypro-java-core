47bb9933e1a2e55893c4c61b050796381cffd8e9
askaretc <askaretc@yandex.ru>
2022-12-25 20:45:24 +0300

    Lesson 4 / Task 2

---
+++ src/pro/sky/java/course2/lesson4/Competing.java
@@ -0,0 +1,10 @@
+package pro.sky.java.course2.lesson4;
+
+public interface Competing {
+
+    void printAmountOfPitStop(int amountOfPitStop);
+
+    void printBestLapTime(int minutesOfBestLapTime);
+
+    void  printMaximumSpeed(int maximumSpeed);
+}
