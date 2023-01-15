ab34adfe370a1a0cb806229a46d484bae2a613a8
askaretc <askaretc@yandex.ru>
2023-01-15 21:14:17 +0300

    Lesson 8 / Task 1

---
+++ src/pro/sky/java/course2/lesson8/driver/DriverD.java
@@ -0,0 +1,7 @@
+package pro.sky.java.course2.lesson8.driver;
+
+public class DriverD extends Driver {
+    public DriverD(String fullName, boolean driverLicense, int workExperience, String category) {
+        super(fullName, driverLicense, workExperience, category);
+    }
+}
