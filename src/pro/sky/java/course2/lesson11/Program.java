e5308bae62c6460191170348529986fa956e1000
askaretc <askaretc@yandex.ru>
2023-02-01 22:38:31 +0300

    Lesson 11 / Task 2

--- src/pro/sky/java/course2/lesson11/Program.java
+++ src/pro/sky/java/course2/lesson11/Program.java
@@ -10,14 +10,23 @@ import java.util.stream.Stream;
 
 public class Program {
     public static void main(String[] args) {
+
         List<Integer> randomIntegerList = new Random().ints(Integer.MIN_VALUE, Integer.MAX_VALUE)
                 .limit(10).boxed().collect(Collectors.toList());
         Stream<Integer> integerStream = randomIntegerList.stream();
         System.out.println(randomIntegerList);
+
+
+        ////////// Task 1 //////////
+        System.out.println("\nTask 1:");
         findMinMax(
                 integerStream,
                 Integer::compareTo,
                 (x, y) -> System.out.println("minimal element: " + x + "\n" + "maximal element: " + y));
+
+        ////////// Task 2 //////////
+        System.out.println("\nTask 2:");
+        printAllEvenNumbers(randomIntegerList);
     }
 
     public static <T> void findMinMax(
@@ -31,6 +40,21 @@ public class Program {
             minMaxConsumer.accept(min.get(), max.get());
         } else {
             minMaxConsumer.accept(null, null);
+        }
+    }
+
+    public static void printAllEvenNumbers(List<Integer> list) {
+        List<Integer> evenNumbersList = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        if (evenNumbersList.size() > 0) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(evenNumbersList.size()).append(" even numbers were found:").append("\n");
+            for (Integer num : evenNumbersList) {
+                sb.append(num).append("\n");
+            }
+            String s = sb.toString();
+            System.out.println(s);
+        } else {
+            System.out.println("Even numbers were not found.");
         }
     }
 }
